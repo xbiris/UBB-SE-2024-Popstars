2f4529e155ebc36af0fb295228d2f0151126b378
darian200205 <darianserban@yahoo.com>
2024-04-15 22:37:26 +0300

    added song uploading

--- SE_project/Services/SongService.cs
+++ SE_project/Services/SongService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace SE_project.Services
 {
@@ -15,10 +11,36 @@ namespace SE_project.Services
 			_songRepo = new SongRepo();
 		}
 
-		public void AddSong(string title, string songUrl, int albumId)
+		public void AddSong(string title, string filePath)
 		{
-			var song = new Song(title, songUrl);
-			_songRepo.AddSong(song, albumId);
+			if (!IsValidAudioFile(filePath))
+			{
+				throw new ArgumentException("Invalid audio file format");
+			}
+
+			if (filePath.Length == 0)
+            {
+				throw new ArgumentException("Please upload a file");
+			}
+
+			if (title.Length == 0)
+            {
+				throw new ArgumentException("Please enter a title");
+			}
+
+			if (_songRepo.GetSongByUrl(filePath) != null || _songRepo.getSongByTitle(title) != null)
+            {
+				throw new ArgumentException("This song already exists");
+            }
+ 
+			var song = new Song(title, filePath);
+			_songRepo.AddSong(song, filePath);
+		}
+
+		private bool IsValidAudioFile(string filePath)
+		{
+			string extension = Path.GetExtension(filePath).ToLower();
+			return extension == ".mp3" || extension == ".wav" || extension == ".ogg";
 		}
 
 		public void DeleteSong(int songId)
